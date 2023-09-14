d035b8d6f44510ed6456c65e322a77bcf95cdabc
rgarcia <rgarcia@innova360.biz>
2023-09-14 16:05:05 -0600

    Ejercicios Semana 1

--- semana-1/Ejercicios_completos.js
+++ semana-1/Ejercicios_completos.js
@@ -1,3 +1,8 @@
+Buenas gente, como va?
+📚 **Ejercicios de la semana 1**
+
+**EJERCICIO 1**
+🔧 Crea una función llamada "sumar" que reciba 2 números por parámetros y retorne el resultado de la suma.
 // LUNES -- Ejercicio 1
 let num_1 = 5;
 let num_2 = 10;
@@ -14,6 +19,10 @@ let ressuma = sumar(num_1, num_2);
 console.log(` ${num_1} + ${num_2} = ${ressuma}`);
 console.log();
 console.log(` - - - - - - - - - - - - - - - - - - -`);
+
+**EJERCICIO 2**
+🔢 Crea una función llamada "multiplicar" que reciba 2 números por parámetros y retorne el resultado de la multiplicación. 
+Además, verifica si ambos parámetros son números; si alguno no lo es, debe devolver el siguiente mensaje: "Ambos parámetros deben ser números." ⚠️
 
 // MARTES -- Ejercicio 2
 const multiplicar = (num_1, num_2) => {
@@ -32,6 +41,13 @@ console.log(` ${num_1} * ${num_3} [ ${tipo} ]  = ${resmulti}`);
 
 console.log();
 console.log(` - - - - - - - - - - - - - - - - - - -`);
+
+
+
+**EJERCICIO 3**
+❄️ Crea una función llamada "esInvierno" que reciba por parámetro el mes y 
+devuelva `true` si es un mes de invierno o `false` en caso contrario. Meses de invierno: junio, julio, agosto.
+
 
 // MIERCOLES - Ejercicio 3
 let mes = "Enero";
@@ -67,6 +83,18 @@ console.log(`Mes a analizar ${mes} = ${esInviernoResponse}`);
 console.log();
 console.log(` - - - - - - - - - - - - - - - - - - -`);
 
+
+
+**EJERCICIO 4**
+🦠 Crea una función llamada "esCovid" que reciba por parámetro dos arrays: uno con los síntomas que posee el 
+paciente y otro con los síntomas del COVID-19. Si coinciden 2 o más síntomas, debe retornar `true`, indicando que es COVID-19; de lo contrario, retorna `false`.
+
+**ARRAYS PARA PROBAR**
+- Síntomas del paciente enfermo: ["fiebre", "dolor muscular", "vómitos"]
+- Síntomas del paciente no enfermo: ["fiebre", "titila ojo", "vómitos"]
+- Síntomas del COVID-19: ["fiebre", "dolor muscular", "pérdida de gusto"]
+
+
 // JUEVES -- Ejercicio 4
 
 let enfermo = ["fiebre", "dolor muscular", "vómitos"];
@@ -96,6 +124,23 @@ console.table(enfermo);
 console.log(`¿ El paciente esta enfermo ? ${es}`);
 console.log();
 console.log(` - - - - - - - - - - - - - - - - - - -`);
+
+
+
+
+
+
+
+**EJERCICIO 5**
+🕒 Crea la función "horarioCursada" que recibe por parámetro la materia y debe devolver el día y la hora en que se cursa. 
+En caso de que la materia no se encuentre en el sistema, retorna el mensaje "MATERIA NO ENCONTRADA." 📅
+
+EJEMPLOS:
+- MATEMÁTICA → JUEVES
+- LENGUA → LUNES
+- GEOGRAFÍA → VIERNES
+- PROGRAMACIÓN → MATERIA NO ENCONTRADA
+
 
 // VIERNES -- Ejercicio 5
 
