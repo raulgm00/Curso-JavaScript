1f73e09278ca3e6c81571a72bba1f6efc0fb6458
rgarcia <rgarcia@innova360.biz>
2023-09-14 16:16:17 -0600

    Mis primeros ejercicio entregables js0

---
+++ js-0/main.js
@@ -0,0 +1,38 @@
+/*Crea 10 variables de tipo string (cadena de texto) y asignale cualquier texto. */
+let variableString = "Hola, mundo!";
+let carcateres = "Caracteres";
+let carcater = "a";
+let parrafo = "Lorem";
+let palabra = "ESTOCOIDE";
+let palabras = "PROYECTO ESTOCOIDE";
+let string1 = "String 1";
+let string2 = "String 2";
+let string3 = "String 3";
+
+
+
+/*Crea 10 variables de tipo number (numero) y asígnale cualquier número.*/
+let variableNumber = 100;
+let cero = 0;
+let uno = 1;
+let dos = 2;
+let tres = 3;
+let cuatro = 4;
+let cinco = 5;
+let seis = 6;
+let siete = 7;
+let ocho = 8;
+let nueve = 9;
+
+
+/*Crea 10 variables de tipo boolean (true/false) y asígnale cualquier valor.*/
+let variableBoolean = true;
+let esReal = true;
+let esNumero = true;
+let esVerdadero = true;
+let esString = true;
+let esChart = true;
+let esPositvo = true;
+let esNegativo = false;
+let esFalsoPositivo = false;
+let bandera = false;
